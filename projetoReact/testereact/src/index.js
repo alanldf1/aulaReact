c6c58766bc87a4a30e977f0b09650d1779f34de8
Alan <alan_souza_nonijana@hotmail.com.br>
2020-09-22 18:02:56 -0300

    aula 22-09

--- projetoReact/testereact/src/index.js
+++ projetoReact/testereact/src/index.js
@@ -4,12 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      marca: 'Ford',
+      carro: 'Ká',
+      cor: 'azul',
+      ano: 2008,
+    };
+  }
+
+  changeColor = () => {
+    this.setState({cor: "Vermelho"})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Meu carro é o {this.state.marca} {this.state.carro}</h1>
+        <p>Alguns detalhes do carro - cor: {this.state.cor} - ano:{this.state.ano}</p>
+        <button onClick={this.changeColor}>Mudar a cor</button>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Car />,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
